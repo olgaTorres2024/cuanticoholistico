c55ff56647809749f8b7f0a3efa2cbbc584d8d71
olgaTorres2024 <olga.torres@gmail.com>
2024-11-20 12:11:35 -0600

    Create days.js

---
+++ js/days.js
@@ -0,0 +1,8 @@
+let fecha = new Date("11/30/2024");
+let msFecha = fecha.getTime();
+
+let parrafoDias = document.querySelector("#days");
+let parrafoHoras = document.querySelector("#s");
+let parrafoMinutos = document.querySelector("#days");
+let parrafoSegundos = document.querySelector("#days");
+let spanFecha = document.querySelector("#days");
